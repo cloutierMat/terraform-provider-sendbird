b33a91b5b18ed13eadf592f61bf69017601049ad
Mathieu Cloutier <cloutier.mat0@gmail.com>
2024-02-09 15:37:33 -0700

    Updated the client to support POST and DELETE

--- internal/client/applications.go
+++ internal/client/applications.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -37,4 +38,42 @@ func (c *SendbirdClient) GetApplication(appId string) (*Application, error) {
 		return nil, err
 	}
 	return &application, nil
+}
+
+func (c *SendbirdClient) CreateApplication(app_name string, region_key string) (*Application, error) {
+	var application Application
+
+	jsonBody := []byte(fmt.Sprintf(`{"app_name": "%s", "region_key": "%s"}`, app_name, region_key))
+	readerBody := bytes.NewReader(jsonBody)
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/applications", c.HostURL), readerBody)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &application)
+	if err != nil {
+		return nil, err
+	}
+	return &application, err
+}
+
+func (c *SendbirdClient) DeleteApplication(appId string) error {
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/applications/%s", c.HostURL, appId), nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.doRequest(req)
+	if err != nil {
+		return err
+	}
+	return nil
 }
